eefae18a7cd8c1890f19d79e9af3ed02510fab09
LeeSohyeon <teabag759@gmail.com>
2023-11-22 13:19:01 +0900

    Initial Commit

---
+++ I Am Rock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rock
+//
+//  Created by 이소현 on 11/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
